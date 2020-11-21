c58895844b97ec4e4807cafa201c4685bae88004
Kenneth Dave <listeringkids@gmail.com>
2020-11-21 12:35:10 +0700

    Success

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,17 +1,27 @@
 import express from 'express';
+import * as middleware from '../../middleware';
 import {
     index,
-    create,
-    remove,
+    register,
+    signin,
     update,
+    remove,
 } from './user.controller';
 
 let userRouter = express.Router();
 
-userRouter.get('/', index)
-userRouter.post('/', create);
-userRouter.delete('/:id', remove);
-userRouter.put('/', update);
+//userRouter.get('/', middleware.isAuthenticated, index);
+//userRouter.post('/', create);
+//userRouter.delete('/:id', remove);
+//userRouter.put('/', update);
+userRouter.get('/', middleware.isAuthenticated, index);
+userRouter.put('/:id', middleware.isAuthenticated, update);
+userRouter.delete('/:id', middleware.isAuthenticated, remove);
+userRouter.post('/register', register);
+userRouter.post('/signin', signin);
     
 
 export default userRouter;
+
+
+
--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,10 +1,13 @@
 import {Request, Response } from 'express';
 import User from './user.model';
+import { IAuthRequestBody } from '../../interface';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import config from '../../environment';
+import userRouter from '.';
 
 export const index = async (req: Request, res: Response) => {
-    console.log("req: ", req);
+    console.log("req: ", req.body);
     try{
         let users = await User.find({});
         return res.status(200).json({data: users});
@@ -13,19 +16,49 @@ export const index = async (req: Request, res: Response) => {
     }
 }
 
-export const create = async (req: Request, res: Response) => {
-    console.log("req: ", req);
+export const register = async (req: Request, res: Response) => {
+    console.log("req: ", req.body);
+    let body = req.body;
+
     try{
-        let body = req.body;
-        let response = await User.create(body);
-        return res.status(200).json({message: "Success", response});
+        const salt = bcrypt.genSaltSync(10);
+        const hashPass = bcrypt.hashSync(body.password, salt);
+        body.salt = salt;
+        body.password = hashPass;
+
+        const response = await User.create(body);
+        return res.status(201).json({success: response});
     }catch(err){
-    return res.status(500).json({message: "Error"});
+        return res.status(201).json({message:err})
+    }
+}
+        
+
+export const signin = async (req: Request, res: Response) => {
+    console.log("req: ",req.body);
+    const body: IAuthRequestBody = req.body;
+
+    try{
+        if(body.nim && body.password){
+            const user = await User.findOne({nim: body.nim});
+
+            if(user){
+                const hashPass = bcrypt.hashSync(body.password, user.salt);
+                if(bcrypt.compareSync(body.password, hashPass)){
+                    const token = jwt.sign({data: user}, config.secret_key, { expiresIn: '1h'});
+                    return res.status(200).json({token: token});
+                }
+            }
+        }else{
+            throw("missing required key in body !")
+        }
+    }catch(err){
+        return res.status(500).json({message: err})
     }
 }
 
 export const remove = async (req: Request, res: Response) => {
-    console.log("req: ", req);
+    console.log("req: ", req.body);
     try{
         let id = req.params.id;
         let body = req.body;
@@ -37,7 +70,7 @@ export const remove = async (req: Request, res: Response) => {
 }
 
 export const update = async (req: Request, res: Response) => {
-    console.log("req: ", req);
+    console.log("req: ", req.body);
     try{
         let id = req.params.id;
         let body = req.body;
